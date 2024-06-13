2efe6068600eccee9f8d208058098e931b03e0ff
mounikasangana0126 <sssmounika5@gmail.com>
2024-06-13 16:58:04 +0530

    updated

--- excelsheet/src/MultipleFiles.js
+++ excelsheet/src/MultipleFiles.js
@@ -5,7 +5,8 @@ import Component from "./Component";
 
 function MultipleFiles() {
   const [value, setValue] = useState([]);
-  const [option,setOption]=useState("react")
+  const [option,setOption]=useState("")
+  const [titles,setTitles]=useState([])
   
   function handleXl(e) {
     const file = e.target.files[0];
@@ -15,15 +16,16 @@ function MultipleFiles() {
       const workbook = XLSX.read(data, { type: "array" });
       const files= workbook.SheetNames;
       const sheetsdata=[]
+      const title=[]
       for(var i=0;i<files.length;i++){
         var sheetName = workbook.SheetNames[i];
+        title.push(sheetName)
         var sheet = workbook.Sheets[sheetName];
-        console.log(sheet);
         var sheetData = XLSX.utils.sheet_to_json(sheet, { header: 1 });
         sheetsdata.push(sheetData)
-       
       }
       setValue(sheetsdata);
+      setTitles(title);
     };
     reader.readAsArrayBuffer(file);
   }
@@ -33,15 +35,10 @@ function MultipleFiles() {
   return (
     <div className="App">
       <input type="file" accept=".xlsx,.xls" onChange={handleXl} />
-      <button type="button" onClick={()=>setOption("react")} >React</button>
-      <button type="button" onClick={()=>setOption("node")}>Nodejs</button>
-      {option === 'react' ? (
-        <Component value={value[0]} />
-      ) : option === 'node' ? (
-        <Component value={value[1]} />
-      ) : (
-        <p>There is no such file</p>
-      )}
+      {titles.map((num,key)=>(
+      <button type="button" key={key} onClick={()=>setOption(key)} >{num}</button>
+      ))} 
+      <Component value={value[option]}/>
     </div>
   );
 }
